655e52bd0fed5d18ede88e4b31bb95e1df6a9bda
Antonin Crha <lord.Rotor@gmail.com>
2011-11-27 16:47:13 +0100

    unused error definition removed

--- application/config/db_config/errors.php
+++ application/config/db_config/errors.php
@@ -19,6 +19,3 @@ $config[ ABST_ERROR_PREFIX ][ 2 ] = 'Autodeclared functions only accept one para
 $config[ ABST_ERROR_PREFIX ][ 3 ] = 'The parameter naming or parameter count does 
                             not corespond to configuration in %sconfig/%s.<br />
                             Error origin: <br />%s';
-
-
-$config[ 'db_errors' ][ 20010 ] = 'Input for package not complete.';
