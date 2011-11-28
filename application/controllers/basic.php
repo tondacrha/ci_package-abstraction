fb0a93882319c4a3b073227af43180c16ea9aa27
Antonin Crha <lord.Rotor@gmail.com>
2011-11-28 22:45:20 +0100

    Add posibility to use php/plsql class as library in models that need to extend another class. If used need to call initialize( $configFileAddress ) before first use.
    libraries/Pl.php - use the CI_Model as parent - cleaner code - prefered version
    libraries/Plsql.php - loads CI class in initialize method - will not support - just for performance tests.
    
    Version in library will probably need some code modifications to backtrace errors correctly

--- application/controllers/basic.php
+++ application/controllers/basic.php
@@ -15,18 +15,41 @@ class basic extends CI_Controller
 
     public function index()
     {
+
         $this->load->model('mpokus');
-        
+        echo '<h4>Using class inharitance:</h4>';
         $iStart = microtime(true);
-        $this->mpokus->getAllPokus(10);
-        // doing two different calls
-        // in between the binds are unset and bind again.
-        // performance tests shows no significant difference when keeping binds
-        echo '<br><br>';
-        $this->mpokus->getAllPokus(5);
+        $this->mpokus->getAllPokus(3);
         $iEnd = microtime(true);
-        echo "<br>";echo $iEnd - $iStart;
-        echo '<br>';echo memory_get_peak_usage(true);
+        echo "<br>Time: ";echo $iEnd - $iStart;
+        echo '<br>Memory usage in bytes: ';echo memory_get_usage();
+        unset($this->mpokus);
+        gc_collect_cycles();
+        
+        
+        $this->load->model('msecond');
+        echo '<br><br>';
+        echo '<h4>Using library:</h4>';
+        $iStart = microtime(true);
+        $this->msecond->getAllPokus(3);
+        $iEnd = microtime(true);
+        unset($this->msecond);
+        echo "<br>Time: ";echo $iEnd - $iStart; 
+        echo '<br>Memory usage in bytes: ';echo memory_get_usage();
+        echo "<br>Better time because db request is already loaded on server. It does not mean, that second solution is faster.";
+        
+        $this->load->model('mthird');
+        echo '<br><br>';
+        echo '<h4>Using library without extending CI_Model:</h4>';
+        $iStart = microtime(true);
+        $this->mthird->getAllPokus(3);
+        $iEnd = microtime(true);
+        echo "<br>Time: ";echo $iEnd - $iStart; 
+        echo '<br>Memory usage in bytes: ';echo memory_get_usage();
+        echo "<br>Better time because db request is already loaded on server. It does not mean, that second solution is faster.";
+        echo '<br>Memory consumption groves in the time. It does not mean that some solution is worse.';
+        
+
         
     }
 
---
+++ application/models/mthird.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Proof of concept.
+ * 
+ * @author Antonin Crha <a.crha@pixvalley.com>
+ */
+class mthird extends CI_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct(  );
+        
+        //load package configuration
+        $this->load->library( 'Plsql' );
+        $sConfigFile = PACKAGE_CONFIG_PATH . 'mpokus' . EXT;
+        $this->plsql->initialize( $sConfigFile );
+    }
+
+    /**
+     * Example:
+     * How easy is to use this Oracle package/procedures abstraction:
+     * --------------------------------------------------------------
+     * Step 0) Prepare configuration file (just once) :-)
+     * Step 1) Prepare input parameters array
+     * Step 2) Pass it to function "declared" in config file $sConfigFile
+     * Step 3) Read output params from variables declared for current object
+     * Step 4) [optinal] Check if cursor contains data with <cursor>->isEmpty
+     * Step 5) [optinal] Fetch data from cursor just like you are used in codeigniter
+     * Step 6) Not done yet? Doing another request to another procedure? 
+     *         Please free the memory with <cursor>->free_result()
+     * 
+     * @author Antonin Crha <a.crha@pixvalley.com>
+     * @param integer $iNum Number of rows to fetch
+     */
+    public function getAllPokus( $iNum )
+    {
+        $aIn  = array( 'LABEL' => 'ahoj', 'CFILE' => 'aaa', 'PN$rownum' => $iNum );
+        
+        if( false === $this->plsql->insert( $aIn ) )
+        {
+            echo $this->plsql->getErrorMessage();
+        }
+        else
+        {
+            if( $this->plsql->PCUR_out->isEmpty() )
+            {
+                echo 'No data';
+            }
+            else
+            {
+                // lets fetch the cursor
+                foreach ($this->plsql->PCUR_out->result() as $row)
+                {
+                    var_dump($row); echo '<br>';
+                }
+            }
+        }
+        // usefull for keepin the memory consumption on acceptable level
+        $this->plsql->PCUR_out->free_result(); 
+        $this->db->trans_commit();
+    }
+
+}
