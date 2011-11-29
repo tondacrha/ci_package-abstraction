b4d5785f3e6b7c056cbcd1f066bb48cf544c64d1
Antonin Crha <lord.Rotor@gmail.com>
2011-11-29 22:02:55 +0100

    desition made. It will be library without extending the CI_Model. Remove other posibilities

--- application/controllers/basic.php
+++ application/controllers/basic.php
@@ -15,20 +15,8 @@ class basic extends CI_Controller
 
     public function index()
     {
-
-        $this->load->model('mpokus');
-        echo '<h4>Using class inharitance:</h4>';
-        $iStart = microtime(true);
-        $this->mpokus->getAllPokus(3);
-        $iEnd = microtime(true);
-        echo "<br>Time: ";echo $iEnd - $iStart;
-        echo '<br>Memory usage in bytes: ';echo memory_get_usage();
-        unset($this->mpokus);
-        gc_collect_cycles();
-        
         
         $this->load->model('msecond');
-        echo '<br><br>';
         echo '<h4>Using library:</h4>';
         $iStart = microtime(true);
         $this->msecond->getAllPokus(3);
@@ -36,20 +24,6 @@ class basic extends CI_Controller
         unset($this->msecond);
         echo "<br>Time: ";echo $iEnd - $iStart; 
         echo '<br>Memory usage in bytes: ';echo memory_get_usage();
-        echo "<br>Better time because db request is already loaded on server. It does not mean, that second solution is faster.";
-        
-        $this->load->model('mthird');
-        echo '<br><br>';
-        echo '<h4>Using library without extending CI_Model:</h4>';
-        $iStart = microtime(true);
-        $this->mthird->getAllPokus(3);
-        $iEnd = microtime(true);
-        echo "<br>Time: ";echo $iEnd - $iStart; 
-        echo '<br>Memory usage in bytes: ';echo memory_get_usage();
-        echo "<br>Better time because db request is already loaded on server. It does not mean, that second solution is faster.";
-        echo '<br>Memory consumption groves in the time. It does not mean that some solution is worse.';
-        
-
         
     }
 
